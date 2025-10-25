ddbb091e83f4f99d09662dbf125a64348d09908c
kev1n999 <sopratesta999@gmail.com>
2025-10-25 15:12:14 -0300

    add time

--- api/resources/generate_ascii.py
+++ api/resources/generate_ascii.py
@@ -20,12 +20,12 @@ def frame_to_ascii(frame):
     width = gray.width
     return "\n".join([ascii_str[i:i+width] for i in range(0, len(ascii_str), width)])
 
-def ascii_generator(route: str, gif_path: str):
+def ascii_generator(route: str, gif_path: str, time=50):
     img = Image.open(gif_path)
     frames = [resize_frame(frame.copy()) for frame in ImageSequence.Iterator(img)]
     ascii_frames = [frame_to_ascii(frame) for frame in frames]
 
-    while True:
+    for x in range(time):
         for ascii_frame in ascii_frames:
             yield ascii_frame
             time.sleep(0.1) 
