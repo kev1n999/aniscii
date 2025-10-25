c66edd1ce70ab45aab024acbb60afe8c78132b2f
kev1n999 <sopratesta999@gmail.com>
2025-10-25 15:07:10 -0300

    add comments in dotenv functions

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import os 
-from dotenv import load_dotenv
+# import os 
+# from dotenv import load_dotenv
 from api.app import create_app
 
 if __name__ == "__main__":
+  # If you wanna to run in localhost
+  # load_dotenv()
+  # port = os.getenv("SERVER_PORT")
+
   app = create_app()
   app.run(host="0.0.0.0", threaded=True)
