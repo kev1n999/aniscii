48f3d9224940bc370df879badec4bf587c1d76ad
kev1n999 <sopratesta999@gmail.com>
2025-10-25 15:25:25 -0300

    add flask limiter

--- api/app.py
+++ api/app.py
@@ -1,13 +1,16 @@
-import os 
-from flask import Flask, Request, Response, json 
-from dotenv import load_dotenv
+from flask import Flask
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from .resources.generate_ascii import ascii_generator
 from .routes.main import main 
 
-load_dotenv()
-
 def create_app():
   app = Flask(__name__)
+  limiter = Limiter(
+    key_func=get_remote_address,
+    app=app,
+    default_limits=["10 per minute"]
+  )
   app.register_blueprint(main)
 
   return app 
