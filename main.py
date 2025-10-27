328818ab3880e8bc8187343dded59a21676b421c
kev1n999 <sopratesta999@gmail.com>
2025-10-27 14:32:13 -0300

    set global app variable

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 # from dotenv import load_dotenv
 from api.app import create_app
 
+app = create_app()
+
 if __name__ == "__main__":
   # If you wanna to run in localhost
   # load_dotenv()
   # port = os.getenv("SERVER_PORT")
 
-  app = create_app()
   app.run(host="0.0.0.0", threaded=True)
